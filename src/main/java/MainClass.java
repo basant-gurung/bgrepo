15f63f9053e038d6f497a08750050e3c6bd6f204
Basant Gurung <basant.gurung@dataguise.com>
2020-11-30 00:01:14 +0530

    commit to branch 2.1

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,6 @@
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello to version - 2.0");
+        System.out.println("Hello to version - 2.1");
     }
-    // this is shelved code
 }
