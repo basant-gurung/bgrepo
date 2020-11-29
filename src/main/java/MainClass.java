f7214634166c2a5d0a16d658646b83668ef6d33a
Basant Gurung <basant.gurung@dataguise.com>
2020-11-29 23:57:00 +0530

    commit to branch 2

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,5 @@
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hello to version - 1.0");
+        System.out.println("Hello to version - 2.0");
     }
 }
