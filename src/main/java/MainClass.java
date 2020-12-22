2b73c25e86dc0a3f1bca0b871b1e302e7b8c0d7c
Basant Gurung <63307641+basant-gurung@users.noreply.github.com>
2020-12-22 17:53:20 +0530

    Update MainClass.java

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,5 +2,6 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello to version - 2.0");
         System.out.println("Hello to version - 2.1");
+        System.out.println("new work to  2.2");
     }
 }
